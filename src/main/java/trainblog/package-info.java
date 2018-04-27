b8ae142425889a89b1cf043c529bab7105293061
Beldon <1047325205@qq.com>
2018-04-27 17:49:21 +0800

    init

---
+++ src/main/java/trainblog/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * @author Beldon
+ * @create 2018-04-27 下午5:35
+ */
+package trainblog;
---
+++ src/main/java/trainblog/servlet/IndexServlet.java
@@ -0,0 +1,23 @@
+package trainblog.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * @author Beldon
+ * @create 2018-04-27 下午5:43
+ */
+@WebServlet(name = "helloUser2", urlPatterns = {"/", "/index"}, loadOnStartup = 1)
+public class IndexServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter out = resp.getWriter();
+        out.write("hello trainblog");
+    }
+}
