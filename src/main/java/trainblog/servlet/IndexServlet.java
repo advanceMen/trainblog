a00cbf6b93f7bb8e96395a63762c8896b8d27ca5
Beldon <1047325205@qq.com>
2018-04-27 18:37:44 +0800

    init

--- src/main/java/trainblog/servlet/IndexServlet.java
+++ src/main/java/trainblog/servlet/IndexServlet.java
@@ -12,12 +12,13 @@ import java.io.PrintWriter;
  * @author Beldon
  * @create 2018-04-27 下午5:43
  */
-@WebServlet(name = "helloUser2", urlPatterns = {"/", "/index"}, loadOnStartup = 1)
+@WebServlet(name = "index", urlPatterns = {"/", "/index"}, loadOnStartup = 1)
 public class IndexServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         PrintWriter out = resp.getWriter();
         out.write("hello trainblog");
+        System.out.println(this);
     }
 }
